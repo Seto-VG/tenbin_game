9a675e3f0a0055249309a3ae8b5d5e5c472b39fd
yuma <hsi47nrooafen@gmail.com>
2024-05-27 14:05:25 +0900

    [update] ステージ数と戻るボタンUI表示

--- tenbin_game/Assets/Script/GameManager.cs
+++ tenbin_game/Assets/Script/GameManager.cs
@@ -15,6 +15,7 @@ public class GameManager : SingletonBehavior<GameManager>
     void Start()
     {
         Initialize();
+        StartCoroutine(UIControlCoroutine());
     }
 
     // Update is called once per frame
@@ -43,7 +44,8 @@ public class GameManager : SingletonBehavior<GameManager>
     // ステージクリア
     public void OnFinishedStage(string whichComplete)
     {
-        // FXManager.instance.PlayFireworks(); // TODO ゲームクリア後のエフェクト再生
+        _isFinishedStage = true;
+
         // Excellentだった場合
         if (whichComplete == "Excellent")
         {
@@ -77,6 +79,11 @@ public class GameManager : SingletonBehavior<GameManager>
         });
     }
 
+    public void ReturnSelectStageScene()
+    {
+        SceneManager.LoadScene("StageSelectScene");
+    }
+
     // 初期化
     private void Initialize()
     {
@@ -86,8 +93,21 @@ public class GameManager : SingletonBehavior<GameManager>
         _nextSceneFlag = false;
     }
 
+    IEnumerator UIControlCoroutine()
+    {
+        UIManager.instance.ActiveStageNum();
+
+        yield return new WaitForSeconds(3.0f);
+
+        UIManager.instance.InactiveStageNum();
+        UIManager.instance.ActiveReturnButton();
+    }
+
     public void SetFinish()
     {
         _isFinishedStage = true;
     }
 }
+
+    
+
--- tenbin_game/Assets/Script/UIManager.cs
+++ tenbin_game/Assets/Script/UIManager.cs
@@ -1,15 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Data.Common;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class UIManager : SingletonBehavior<UIManager>
 {
+    private int stageId = GameData.stageId;
     [SerializeField] private GameObject _excellentText;
     [SerializeField] private GameObject _greatText;
     [SerializeField] private GameObject _failedText;
     [SerializeField] private GameObject _nextStageText;
     [SerializeField] private GameObject _returnSelectStageText;
-    // Start is called before the first frame update
+    [SerializeField] private GameObject _returnButton;
+    [SerializeField] private List<GameObject> _stageNumTextList = new List<GameObject>();
+    private GameObject _stageNumberInstance;
+    [SerializeField] private RectTransform canvasTransform;
+
+    private void Start()
+    {
+        // stageIdに対応したステージ数のUIをインスタンス化する
+        _stageNumberInstance = Instantiate(_stageNumTextList[stageId], canvasTransform);
+    }
+
     public void ActiveExcellentText()
     {
         _excellentText.SetActive(true);
@@ -33,5 +46,20 @@ public class UIManager : SingletonBehavior<UIManager>
     public void ActiveReturnSelectStageText()
     {
         _returnSelectStageText.SetActive(true);
+    }
+    
+    public void ActiveReturnButton()
+    {
+        _returnButton.SetActive(true);
+    }
+
+    public void ActiveStageNum()
+    {
+        _stageNumTextList[stageId].SetActive(true);
+    }
+
+    public void InactiveStageNum()
+    {
+        _stageNumberInstance.SetActive(false);
     }
 }
