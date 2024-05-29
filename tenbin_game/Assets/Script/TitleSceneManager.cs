babbf1afab8cd128d451c7183319740c4bdfe5ef
Seto-VG <131129286+Seto-VG@users.noreply.github.com>
2024-05-29 23:16:51 +0900

    [fix] タイトルシーンの修正

--- tenbin_game/Assets/Script/TitleSceneManager.cs
+++ tenbin_game/Assets/Script/TitleSceneManager.cs
@@ -12,50 +12,46 @@ public class TitleSceneManager : MonoBehaviour
     // ---------- ゲームオブジェクト参照変数宣言 ----------
     // ---------- プレハブ ----------
     // ---------- プロパティ ----------
-    // [SerializeField] private TextMeshProUGUI _titleText = default;
-    // [SerializeField] private TextMeshProUGUI _enterText = default;
-    // [SerializeField] private Image _image = default;
+    [SerializeField] private TextMeshProUGUI _enterText = default;
+    [SerializeField] private new GameObject gameObject;
     // ---------- クラス変数宣言 ----------
     // ---------- インスタンス変数宣言 ----------
-    // private Vector3 _initialTitleTextPosition = default;
+    private Vector3 _initialTitleTextPosition = default;
     // ---------- コンストラクタ・デストラクタ ----------
     // ---------- Unity組込関数 ----------
     private void Start()
     {
-        // StartFloatingTitleText();
-        // FadeAnimationEnterText(false);
+        StartFloatingTitleText();
+        FadeAnimationEnterText(false);
     }
 
     private void Update()
     {
-        
+
     }
 
     public void StartGame()
     {
-        if (Input.GetKeyDown(KeyCode.Return))
-        {
-            SceneManager.LoadScene("StageSelectScene");
-        }
+        SceneManager.LoadScene("StageSelectScene");
     }
     // ---------- Public関数 ----------
     // ---------- Protected関数 ----------
     // ---------- Private関数 ----------
 
-    // private void StartFloatingTitleText()
-    // {
-    //     _initialTitleTextPosition = _titleText.transform.localPosition;
-    //     _titleText.transform
-    //         .DOLocalMoveY(_initialTitleTextPosition.y + 50.0f, 3.0f)
-    //         .SetEase(Ease.InOutSine)
-    //         .SetLoops(-1, LoopType.Yoyo);
-    // }
+    private void StartFloatingTitleText()
+    {
+        _initialTitleTextPosition = gameObject.transform.localPosition;
+        gameObject.transform
+            .DOLocalMoveY(_initialTitleTextPosition.y + 50.0f, 3.0f)
+            .SetEase(Ease.InOutSine)
+            .SetLoops(-1, LoopType.Yoyo);
+    }
 
-    // private void FadeAnimationEnterText(bool bIn)
-    // {
-    //     _enterText
-    //         .DOFade(bIn ? 1.0f : 0.0f, 1.0f)
-    //         .SetEase(Ease.InOutSine)
-    //         .OnComplete(() => FadeAnimationEnterText(!bIn));
-    // }
+    private void FadeAnimationEnterText(bool bIn)
+    {
+        _enterText
+            .DOFade(bIn ? 1.0f : 0.0f, 1.0f)
+            .SetEase(Ease.InOutSine)
+            .OnComplete(() => FadeAnimationEnterText(!bIn));
+    }
 }
